dbee780eaec5bf8dc8eda69bb9fc045a8cb797b8
Dominik Gils <DominikGils@gmail.com>
2017-07-25 15:36:30 +0200

    Refactor WLAN. Move Config to WLAN

--- Code/WLAN.py
+++ Code/WLAN.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, PIPE
+import PyToSh
 from ErrorClass import InterfaceError, ConnectionError
 
 
@@ -8,8 +8,7 @@ from ErrorClass import InterfaceError, ConnectionError
 def check_connection(host, count=4):
 	""" If wlan interface is up, try to ping the host for count times """
 	command = ["ping", "-c", count, host]
-	process = Popen(command, stdout=PIPE)
-	output = process.communicate()[0]
+	output = PyToSh.popen_comm(command)
 	# Todo more generic way?
 	if output.__contains__("not found"):
 		raise InterfaceError("Host not found")
@@ -22,8 +21,7 @@ def check_connection(host, count=4):
 def check_interface():
 	""" Checks if the interface is configured """
 	command = ["iwconfig"]
-	process = Popen(command, stdout=PIPE)
-	output = process.communicate()[0]
+	output = PyToSh.popen_comm(command)
 	if output.__contains__("unassociated"):
 		raise InterfaceError("Interface not properly configured")
 	return
@@ -33,10 +31,28 @@ def restart_interface(iface):
 	""" Restarts the Interface called iface """
 	# see comment above about sudo
 	# Todo check -f option
-	command_down = ["sudo", "ifdown", "-f", iface]
-	command_up = ["sudo", "ifup", iface]
-	if_down = Popen(command_down)
-	if_down.wait()
-	if_up = Popen(command_up)
-	if_up.wait()
+	command_down = ["ifdown", "-f", iface]
+	command_up = ["ifup", iface]
+	PyToSh.sudo_popen_wait(command_down)
+	PyToSh.sudo_popen_wait(command_up)
+	return
+
+
+""" Configuration of the RED Brick to use WLAN """
+
+
+def wpa_supplicant():
+	""" Creates the wpa_supplicant configuration"""
+	# Todo
+	config_string = ""
+	supplicant_file = "/etc/wpa_supplicant/wpa_supplicant.conf"
+	cmd_one = ["echo", config_string]
+	cmd_two = ["tee", "sudo", "nano", supplicant_file]
+	PyToSh.popoen_pipe(cmd_one, cmd_two)
+	return
+
+
+def interfaces():
+	""" Extends the interface file"""
+	# Todo
 	return
