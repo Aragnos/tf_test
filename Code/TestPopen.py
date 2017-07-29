e25b0f563bfd294595024efdda759813311a086e
Dominik GIls <DominikGils@gmail.com>
2017-07-29 13:00:26 +0200

    Fixed Ping

--- Code/TestPopen.py
+++ Code/TestPopen.py
@@ -1,11 +1,18 @@
 import PyToSh
-
+from ErrorClass import ConnectionError 
 def test():
     cmd1 = ["ping", "-c", "4", "127.0.0.1"]
     cmd2 = ["grep", "packet loss"]
     output = PyToSh.popen_pipe(cmd1, cmd2)
-    output = output.split(",")[2].split("%")[0]
-    if int(output) <= 50:
-        print(output)
+    #output = "Host not found"
+    try:
+        output = output.split(",")[2].split("%")[0]
+        if int(output) < 100:
+            print(output)
+        else:
+            raise
+    except:
+        raise ConnectionError("Ping failed")
+    return
 
 test()
