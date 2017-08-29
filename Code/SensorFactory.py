b060fac2b8e648b4ec950133b60935b3ca85e6a9
Dominik Gils <DominikGils@gmail.com>
2017-08-29 19:17:37 +0200

    Some new things

---
+++ Code/SensorFactory.py
@@ -0,0 +1,2 @@
+"""  Try to create dummy sensors, sending data if asked. Use this to emulate sensors. Necessary, possible? """
+# TODO
---
+++ Code/TestFallout.py
@@ -0,0 +1,24 @@
+""" Test file. If DB-connection is lost,  save sensor data to file """
+
+
+import Datenbank as DB
+import ErrorClass
+
+database_host = "localhost"
+database_port = 3306
+user = "Red"
+passwd = "red-brick42"
+database = "tf"
+
+try:
+	conn = DB.connect_database(database_host, database_port, user, passwd, database)
+	# any sensor data in files? Yes: Save these to db. No: continue.
+	# TODO
+	sensor_file = open('Test.txt', 'a')
+	# save sensor data to database
+except ErrorClass.DatabaseError as e:
+	print(e.msg)
+	# TODO
+	# save sensor data to file
+	sensor_file = open('Test.txt', 'a')
+	sensor_file.close()
