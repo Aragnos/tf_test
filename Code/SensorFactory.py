f9b03c0a403b0242b0b9c5a2bc9182ebc1e69d3e
Dominik Gils <DominikGils@gmail.com>
2017-08-29 22:03:26 +0200

    Sensor FActory

--- Code/SensorFactory.py
+++ Code/SensorFactory.py
@@ -1,15 +1,11 @@
 from abc import ABCMeta, abstractmethod
 """  Try to create dummy sensors, sending data if asked. Use this to emulate sensors. Necessary, possible? """
 # TODO
-
+# TODO two methods for barometer
 
 class Sensor:
 	__metaclass__ = ABCMeta
 	# Base class for sensors
-
-	@abstractmethod
-	def __init__(self):
-		pass
 
 	@abstractmethod
 	def get_value(self):
@@ -18,17 +14,54 @@ class Sensor:
 
 class AmbientLight(Sensor):
 
-	def __init__(self):
-		super(AmbientLight, self).__init__()
-		pass
+	def get_value(self):
+		return 10
+
+
+class Barometer(Sensor):
 
 	def get_value(self):
-		return 10.
+		return 100
 
-ab = AmbientLight()
-x = ab.get_value()
-print(x)
-sensors = { "ambient_light" : ab}
+
+class Humidity(Sensor):
+
+	def get_value(self):
+		return 200
+
+
+class Moisture(Sensor):
+	def get_value(self):
+		return 300
+
+
+class Temperature(Sensor):
+	def get_value(self):
+		return 400
+
+
+class ThermoCouple(Sensor):
+	def get_value(self):
+		return 1000
+
+ambient = AmbientLight()
+barometer = Barometer()
+humidity = Humidity()
+moisture = Moisture()
+temperature = Temperature()
+thermo = ThermoCouple()
+
+
+sensors = {
+		"ambient_light": ambient,
+		"barometer": barometer,
+		"humidity": humidity,
+		"moisture": moisture,
+		"temperature": temperature,
+		"thermo_couple": thermo}
 
 y = sensors["ambient_light"].get_value()
 print(y)
+
+for sensor in sensors:
+	print(sensors[sensor].get_value())
