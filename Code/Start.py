33ffd7020cab935dec353724f2c35e30b31cf989
Dominik Gils <DominikGils@gmail.com>
2017-09-09 16:26:08 +0200

    Flow Chart

--- Code/Start.py
+++ Code/Start.py
@@ -8,19 +8,23 @@ import config
 # Todo: test all
 # todo opt: LCD Script
 
-
+# Database stuff
 database_host = config.DATABASE_HOST
 database_port = config.DATABASE_PORT
 user = config.DATABASE_USER
 password = config.DATABASE_PASSWORD
 database = config.DATABASE_NAME
+# Build dictionary from bricklet UIDS
+
 
 # connect sensors
+# LCD connected?
+# 	Success: Wait for user input, Failure: continue
 # WLAN available?
 # Loop forever
 # 	get sensor values
 # 	Try to connect to DB
-# 	Success: Save sensor values to DB, Failure: Save sensor values to SD Card
+# 		Success: Save sensor values to DB, Failure: Save sensor values to SD Card
 # 	Sleep
 try:
 	conn = DB.connect_database(host=database_host, port=database_port, user=user, passwd=password, db=database)
--- Code/config.py
+++ Code/config.py
@@ -1,8 +1,27 @@
+# --------------------------------------------------
+# WLAN Settings
+# --------------------------------------------------
 WLAN_USER = ""
 WLAN_PASSWORD = ""
+# --------------------------------------------------
+# Database Settings
+# --------------------------------------------------
 DATABASE_HOST = "localhost"
 DATABASE_PORT = "3306"
 DATABASE_NAME = "tf"
 DATABASE_USER = "Red"
 DATABASE_PASSWORD = "red-brick42"
+# --------------------------------------------------
+# Linux Settings
+# --------------------------------------------------
 ROOT_PASSWORD = "tf"
+# --------------------------------------------------
+# Bricklet UIDS
+# --------------------------------------------------
+UID_AMBIENT = "yBG"  # UID of Ambient Light Bricklet 2.0
+UID_BAROMETER = "ytN"  # UID for Barometer Bricklet
+UID_HUMIDITY = "Cd7"  # UID for Humidity Bricklet
+UID_LCD = "BFX"  # UID of LCD Display
+UID_MOISTURE = "zSG"  # UID for Moisture Bricklet
+UID_TEMPERATURE = "zbS"  # UID for Temperature Bricklet
+UID_THERMOCOUPLE = "B8k"  # UID for Thermocouple Bricklet
